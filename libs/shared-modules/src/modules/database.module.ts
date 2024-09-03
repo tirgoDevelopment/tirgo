e4313db815dfb02bc5ad6899886532ae2329e137
fazliddinnarkhajayev <fazliddinnarkhajayev@gmail.com>
2024-09-03 14:15:49 +0500

    add

--- libs/shared-modules/src/modules/database.module.ts
+++ libs/shared-modules/src/modules/database.module.ts
@@ -83,7 +83,7 @@ const entities = [
             password: 'postgres',
             username: 'postgres',
             entities: entities,
-            database: 'postgres',
+            database: 'test',
             synchronize: true,
         }),
     ],
